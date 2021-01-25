1cbc7e558a7a88c43ed6fa71f5884d64dc5342df
Simon Hoyos <shmesa23@gmail.com>
2021-01-25 09:36:41 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+const greet = 'hola mundo';
+
+const arr = [1, 2, 3];
+
+arr
+  .map((el) => el * 3 + 5)
+  .filter((el) => el > 0)
+  .map((el) => el * 4 + 10)
+  .filter((el) => el > 0);
